141c585148247a9471a6aa776873c5ebd84ae510
unknown <nagaraju.vugile2@gmail.com>
2022-11-28 06:43:12 +0530

    added pagination for bookings

--- src/components/MyBookings.jsx
+++ src/components/MyBookings.jsx
@@ -1,61 +1,132 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BiPencil, BiTrash } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Button, Col, Container, Row, Spinner } from "reactstrap";
-import { myBookings, selectedDataDisplay, selectedReschedulerDetails, setActiveTab } from "../store/scheduler/actions";
+import {
+  Button,
+  Col,
+  Container,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Row,
+  Spinner,
+} from "reactstrap";
+
+import {
+  myBookings,
+  selectedDataDisplay,
+  selectedReschedulerDetails,
+  setActiveTab,
+} from "../store/scheduler/actions";
 import Login from "./Login";
 import NavBar from "./NavBar";
-
 
 const MyBookings = () => {
   const navigate = useNavigate();
   let dispatch = useDispatch();
-
+  // const [start, setStart] = useState(0);
+  // const [end, setEnd] = useState(5);
+  const [displayData, setDisplayData] = useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
   const userProfile = useSelector(
     (state) => state?.availabilitiesReducer?.userProfile
   );
-
+  
   const myBookingsData = useSelector(
     (state) => state?.availabilitiesReducer?.myBookings?.pxResults
   );
 
+  const pageSize = 10;
+  const pagesCount = Math.ceil(myBookingsData?.length / pageSize);
   const loader = useSelector(
     (state) => state?.availabilitiesReducer?.myBookingsLoader
   );
 
-    const handleReschedule = (id,date, type, guid, pxObjClass, StartTime)=>{
-      dispatch(selectedDataDisplay(new Date(date)));
-      dispatch(selectedReschedulerDetails({id, date, type, guid, pxObjClass, StartTime}))
-      navigate("/scheduler/re-schedule/"+id+"?Type="+type);
-    }
+  const handleReschedule = (id, date, type, guid, pxObjClass, StartTime) => {
+    dispatch(selectedDataDisplay(new Date(date)));
+    dispatch(
+      selectedReschedulerDetails({
+        id,
+        date,
+        type,
+        guid,
+        pxObjClass,
+        StartTime,
+      })
+    );
+    navigate("/scheduler/re-schedule/" + id + "?Type=" + type);
+  };
 
-    const handleCancel = (id,date, type, guid, pxObjClass, StartTime)=>{
-      dispatch(selectedReschedulerDetails({id, date, type, guid, pxObjClass, StartTime}))
-      navigate("/scheduler/cancel/"+id);
-    }
-    
+  const handleCancel = (id, date, type, guid, pxObjClass, StartTime) => {
+    dispatch(
+      selectedReschedulerDetails({
+        id,
+        date,
+        type,
+        guid,
+        pxObjClass,
+        StartTime,
+      })
+    );
+    navigate("/scheduler/cancel/" + id);
+  };
+
+  const updateDisplay = () => {
+    // const data = myBookingsData?.filter(
+    //   (item, index) => index < end && index >= start
+    // );
+    // console.log(
+    //   "********************",
+    //   myBookingsData
+    //     ?.slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+    //     .map((data, i) => data)
+    // );
+    setDisplayData(
+      myBookingsData
+        ?.slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+        .map((data, i) => data)
+    );
+  };
+
   useEffect(() => {
     dispatch(myBookings("chandan_palamakula"));
-    dispatch(setActiveTab("2"))
+    dispatch(setActiveTab("2"));
   }, [dispatch]);
 
-  const storedUser = sessionStorage.getItem('userProfile');
   useEffect(() => {
-    if (!userProfile && storedUser ==='null') {
+    updateDisplay();
+  }, [myBookingsData, currentPage]);
+
+  const storedUser = sessionStorage.getItem("userProfile");
+  useEffect(() => {
+    if (!userProfile && storedUser === "null") {
       navigate("/login");
     }
-});
+  });
 
-if (loader) {
-  return (
-    <div className="loader">
-      <Spinner color="dark">
-        Loading...
-      </Spinner>
-    </div>
-  );
-}
+  // const renderPagination = () => {
+  //   console.log("----------==============");
+  //   [...Array(pagesCount)].map((page, i) => (
+  //     <PaginationItem active={i === 2} key={i}>
+  //       <PaginationLink onClick={(e) => console.log(e, i)} href="#">
+  //         {i + 1}
+  //       </PaginationLink>
+  //     </PaginationItem>
+  //   ));
+  // };
+
+  const handlePageClick = (e, index) => {
+    e.preventDefault();
+    setCurrentPage(index);
+  };
+  if (loader) {
+    return (
+      <div className="loader">
+        <Spinner color="dark">Loading...</Spinner>
+      </div>
+    );
+  }
 
   return (
     <Container className="container-scheduler">
@@ -67,90 +138,118 @@ if (loader) {
         </Col>
       </Row>
       {myBookingsData && (
-        <Row>
-          <Row style={{ paddingBottom: "20px" }} className="table-row-style">
-            <Col className="info-sub-header">S.no</Col>
-            <Col className="info-sub-header">Type</Col>
-            <Col className="info-sub-header">Scheduled date</Col>
-            <Col className="info-sub-header">Start time</Col>
-            <Col className="info-sub-header">End time</Col>
-            <Col className="info-sub-header" style={{ minWidth: "21%" }}>
-              Action
-            </Col>
-          </Row>
-          {myBookingsData?.map((item, index) => (
-            <Row
-              key={index}
-              style={{ paddingBottom: "5px" }}
-              className="table-row-style"
-            >
-              <Col>{index + 1}</Col>
-              <Col>{item.SchedulerType}</Col>
-              <Col>{item.ScheduledDate}</Col>
-              <Col>
-                {item.StartTime.split(":")[0]}:{item.StartTime.split(":")[1]}
-              </Col>
-              <Col>
-                {item.EndTime.split(":")[0]}:{item.EndTime.split(":")[1]}
-              </Col>
-              <Col style={{ minWidth: "21%" }}>
-                <Button
-                  color="info"
-                  onClick={() =>
-                    handleReschedule(
-                      item.SchedulerEmailID,
-                      item.ScheduledDate,
-                      item.SchedulerType,
-                      item.pyGUID,
-                      item.pxObjClass,
-                      item.StartTime
-                    )
-                  }
-                  style={{ marginRight: "5px" }}
-                  className="action-button-table"
-                >
-                  Reschedule 
-                </Button>
-                <BiPencil className="edit-pencil"  onClick={() =>
-                    handleReschedule(
-                      item.SchedulerEmailID,
-                      item.ScheduledDate,
-                      item.SchedulerType,
-                      item.pyGUID,
-                      item.pxObjClass,
-                      item.StartTime
-                    )
-                  }/>
-                <Button
-                  color="danger"
-                  onClick={() =>
-                    handleCancel(
-                      item.SchedulerEmailID,
-                      item.ScheduledDate,
-                      item.SchedulerType,
-                      item.pyGUID,
-                      item.pxObjClass,
-                      item.StartTime
-                    )
-                  }
-                  className="action-button-table"
-                >
-                  Cancel 
-                </Button>
-                <BiTrash className="cancel-trash" onClick={() =>
-                    handleCancel(
-                      item.SchedulerEmailID,
-                      item.ScheduledDate,
-                      item.SchedulerType,
-                      item.pyGUID,
-                      item.pxObjClass,
-                      item.StartTime
-                    )
-                  }/>
+        <>
+          <Row>
+            <Row style={{ paddingBottom: "20px" }} className="table-row-style">
+              <Col className="info-sub-header">S.no</Col>
+              <Col className="info-sub-header">Type</Col>
+              <Col className="info-sub-header">Scheduled date</Col>
+              <Col className="info-sub-header">Start time</Col>
+              <Col className="info-sub-header">End time</Col>
+              <Col className="info-sub-header" style={{ minWidth: "21%" }}>
+                Action
               </Col>
             </Row>
-          ))}
-        </Row>
+            {displayData?.map((item, index) => (
+              <Row
+                key={index}
+                style={{ paddingBottom: "5px" }}
+                className="table-row-style"
+              >
+                <Col>{index + 1}</Col>
+                <Col>{item.SchedulerType}</Col>
+                <Col>{item.ScheduledDate}</Col>
+                <Col>
+                  {item.StartTime.split(":")[0]}:{item.StartTime.split(":")[1]}
+                </Col>
+                <Col>
+                  {item.EndTime.split(":")[0]}:{item.EndTime.split(":")[1]}
+                </Col>
+                <Col style={{ minWidth: "21%" }}>
+                  <Button
+                    color="info"
+                    onClick={() =>
+                      handleReschedule(
+                        item.SchedulerEmailID,
+                        item.ScheduledDate,
+                        item.SchedulerType,
+                        item.pyGUID,
+                        item.pxObjClass,
+                        item.StartTime
+                      )
+                    }
+                    style={{ marginRight: "5px" }}
+                    className="action-button-table"
+                  >
+                    Reschedule
+                  </Button>
+                  <BiPencil
+                    className="edit-pencil"
+                    onClick={() =>
+                      handleReschedule(
+                        item.SchedulerEmailID,
+                        item.ScheduledDate,
+                        item.SchedulerType,
+                        item.pyGUID,
+                        item.pxObjClass,
+                        item.StartTime
+                      )
+                    }
+                  />
+                  <Button
+                    color="danger"
+                    onClick={() =>
+                      handleCancel(
+                        item.SchedulerEmailID,
+                        item.ScheduledDate,
+                        item.SchedulerType,
+                        item.pyGUID,
+                        item.pxObjClass,
+                        item.StartTime
+                      )
+                    }
+                    className="action-button-table"
+                  >
+                    Cancel
+                  </Button>
+                  <BiTrash
+                    className="cancel-trash"
+                    onClick={() =>
+                      handleCancel(
+                        item.SchedulerEmailID,
+                        item.ScheduledDate,
+                        item.SchedulerType,
+                        item.pyGUID,
+                        item.pxObjClass,
+                        item.StartTime
+                      )
+                    }
+                  />
+                </Col>
+              </Row>
+            ))}
+          </Row>
+          <Row>
+            <Pagination aria-label="page-navigation" className="page-navigation">
+              <PaginationItem disabled={currentPage <= 0}>
+                <PaginationLink previous onClick={e => handlePageClick(e, currentPage - 1)} href="#" />
+              </PaginationItem>
+              {[...Array(pagesCount)].map((page, i) => (
+                <PaginationItem active={i === currentPage} key={i}>
+                  <PaginationLink
+                    onClick={(e) => handlePageClick(e, i)}
+                    href="#"
+                  >
+                    {i + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+              <PaginationItem disabled={currentPage >= pagesCount - 1}>
+                <PaginationLink next  onClick={e => handlePageClick(e, currentPage + 1)} href="#" />
+              </PaginationItem>
+            </Pagination>
+          </Row>
+        </>
       )}
       {!myBookingsData && <div>No bookins found</div>}
       {/* <Row>
